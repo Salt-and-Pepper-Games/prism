0798f9e34b6fb5618fc0ad06c054b61cd125d06f
sgrund14 <sgrund@oberlin.edu>
2017-09-04 22:53:57 -0400

    added animation to splash

---
+++ docs/js/index.js
@@ -0,0 +1,81 @@
+"use strict";
+
+const paperInstance = new paper.PaperScope();
+const canvas = document.getElementById('paper-canvas');
+const context = canvas.getContext('2d');
+context.canvas.width  = window.innerWidth;
+context.canvas.height = window.innerHeight;
+paperInstance.setup(canvas);
+const pathArray = [];
+const directions = ['up', 'left', 'down', 'right'];
+const colors = ['#FF6978', '#94EA54', '#3885F9', '#F4EE36', '#C85ED8', '#4CEDDE', '#464B63', '#FFF8F4'];
+
+// https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+const hexToRgb = hex => {
+    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+        return r + r + g + g + b + b;
+    });
+
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+};
+
+// Place the instances of the symbol:
+for (let i = 0; i < 50; i++) {
+	// The center position is a random point in the view:
+    const rand = paperInstance.Point.random();
+    const centX = rand.x * paperInstance.view.size.width;
+    const centY = rand.y * paperInstance.view.size.height;
+    const center = new paperInstance.Point(centX, centY);
+    const rectangleSize = Math.random() * 25 + 25;
+    console.log(rectangleSize);
+	const rectangle = new paperInstance.Rectangle(new paperInstance.Point(centX, centY), new paperInstance.Point(centX + rectangleSize, centY + rectangleSize));
+	const cornerSize = new paperInstance.Size(7.5, 7.5);
+	const path = new paperInstance.Path.RoundRectangle(rectangle, cornerSize);
+	const randomColorHex = colors[Math.floor(Math.random()*colors.length)];
+	const colorRGB = hexToRgb(randomColorHex);
+	path.fillColor = new paperInstance.Color(colorRGB.r/255,colorRGB.g/255,colorRGB.b/255,.5);
+	const pathObj = {
+		path: path,
+		direction: directions[Math.floor(Math.random()*directions.length)],
+		speed: Math.random() * 500 + 100
+	};
+	pathArray.push(pathObj);
+}
+
+paperInstance.view.onFrame = () => {
+	pathArray.forEach(item => {
+		switch (item.direction) {
+			case 'up':
+				item.path.position.y -= item.path.bounds.height / item.speed;
+				break;
+			case 'down':
+				item.path.position.y += item.path.bounds.height / item.speed;
+				break;
+			case 'left': 
+				item.path.position.x -= item.path.bounds.width / item.speed;
+				break;
+			case 'right':
+			default:
+				item.path.position.x += item.path.bounds.width / item.speed;
+				break;
+		}
+
+        // wrap screen edges
+        if (item.path.bounds.left > paperInstance.view.size.width) {
+            item.path.position.x -= paperInstance.view.size.width + item.path.bounds.width
+        } else if (item.path.bounds.top > paperInstance.view.size.height) {
+            item.path.position.y -= paperInstance.view.size.height + item.path.bounds.height;
+        } else if (item.path.bounds.right < 0) {
+            item.path.position.x += paperInstance.view.size.width + item.path.bounds.width;
+        } else if (item.path.bounds.bottom < 0) {
+            item.path.position.y += paperInstance.view.size.height + item.path.bounds.height;
+        }
+	});
+};
