d2f02f0a6399e75d76912a192bd8eff104ea3bc1
sgrund14 <sgrund@oberlin.edu>
2017-09-20 12:53:47 -0400

    demo track plays on load

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-
+import { Howl } from 'howler';
 import MainScreenContainer from './ui/containers/MainScreenContainer.jsx'
 import GameAreaContainer from './ui/containers/GameAreaContainer.jsx';
-
+import demoTrack from './audio/demo_blue.mp3'
 
 class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.mainTrack = new Howl({
+		  src: [demoTrack],
+		  loop: true,
+		  html5: true
+		});
+	}
+	componentDidMount() {
+		this.mainTrack.play();
+	}
 	render() {
 		return (
 			<div>
