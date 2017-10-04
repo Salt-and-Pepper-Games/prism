4f7a624ca62b383577f2d9505e44f5f6e94ed23b
sgrund14 <sgrund@oberlin.edu>
2017-10-04 17:34:57 -0400

    mobile audio tweaks

--- src/utils/AudioManager.js
+++ src/utils/AudioManager.js
@@ -5,14 +5,11 @@ import GameAudioM4A from '../audio/GameAudio.m4a';
 import AudioInfo from '../audio/GameAudio.json';
 import { Howl } from 'howler';
 
-const mq = window.matchMedia("(orientation: portrait)");
-const mq2 = window.matchMedia("only screen and (max-width: 800px) and (orientation: landscape)");
-
 const spriteMap = {};
 for (const sprite in AudioInfo.spritemap) {
 	spriteMap[sprite] = [
 		AudioInfo.spritemap[sprite].start * 1000,
-		mq.matches || mq2.matches ? AudioInfo.spritemap[sprite].end * 1000 : (AudioInfo.spritemap[sprite].end - AudioInfo.spritemap[sprite].start) * 1000,
+		(AudioInfo.spritemap[sprite].end - AudioInfo.spritemap[sprite].start) * 1000,
 		AudioInfo.spritemap[sprite].loop
 	];
 }
